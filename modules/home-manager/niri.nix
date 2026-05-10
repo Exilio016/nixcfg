64a8bb4885f63bcc2b191140db8f88fa5fc26c11
Bruno Flávio Ferreira <him@brunoflaviof.com>
2026-05-10 15:48:52 -0300

    Use pywal on swaylock config

--- modules/home-manager/niri.nix
+++ modules/home-manager/niri.nix
@@ -245,46 +245,6 @@
     programs.swaylock = {
         enable = true;
         package = pkgs.swaylock-effects;
-        settings = {
-            daemonize = true;
-            show-failed-attempts = true;
-            clock = true;
-            screenshot = true;
-            effect-blur="13x13";
-            effect-vignette="0.5:0.5";
-            color="6272A4";
-            font="Inter";
-            indicator = true;
-            indicator-radius="200";
-            indicator-thickness="20";
-            line-color="282A36";
-            ring-color="BD93F9";
-            inside-color="282A36";
-            key-hl-color="50FA7B";
-            separator-color="00000000";
-            text-color="F8F8F2";
-            text-caps-lock-color="";
-            line-ver-color="BD93F9";
-            ring-ver-color="BD93F9";
-            inside-ver-color="282A36";
-            text-ver-color="8BE9FD";
-            ring-wrong-color="FF5555";
-            text-wrong-color="FF5555";
-            inside-wrong-color="282A36";
-            inside-clear-color="282A36";
-            text-clear-color="8BE9FD";
-            ring-clear-color="8BE9FD";
-            line-clear-color="8BE9FD";
-            line-wrong-color="282A36";
-            bs-hl-color="8BE9FD";
-            grace="2";
-            grace-no-mouse = true;
-            grace-no-touch = true;
-            datestr="%a, %B %e";
-            timestr="%I:%M %p";
-            fade-in=0.4;
-            ignore-empty-password = true;
-        };
     };
 
     xdg.configFile."niri/config.kdl" = {
@@ -535,6 +495,7 @@
         "L ${config.home.homeDirectory}/.config/Kvantum/Pywal/Pywal.kvconfig - - - -  ${config.home.homeDirectory}/.cache/wal/Kvantum.kvconfig"
         "L ${config.home.homeDirectory}/.config/Kvantum/Pywal/Pywal.svg - - - -  ${config.home.homeDirectory}/.cache/wal/Kvantum.svg"
         "L ${config.home.homeDirectory}/.config/mako/config - - - -  ${config.home.homeDirectory}/.cache/wal/mako.conf"
+        "L ${config.home.homeDirectory}/.config/swaylock/config - - - -  ${config.home.homeDirectory}/.cache/wal/swaylock.conf"
         "L ${config.home.homeDirectory}/.local/share/themes/FlatColor/gtk-2.0/gtkrc - - - -  ${config.home.homeDirectory}/.config/wpg/templates/gtk2"
         "L ${config.home.homeDirectory}/.local/share/themes/FlatColor/gtk-3.0/gtk.css - - - -  ${config.home.homeDirectory}/.config/wpg/templates/gtk3"
         "L ${config.home.homeDirectory}/.local/share/themes/FlatColor/gtk-4.0/gtk.css - - - -  ${config.home.homeDirectory}/.config/wpg/templates/gtk4"
